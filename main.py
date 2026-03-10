e0e8ce6ba00ebe9bcbc244424f45e7c8cfe0209c
Drake <drakedgarnsey@gmail.com>
2026-03-10 16:45:54 -0400

    added a player class that draws to the screen

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import *
 
 def main():
-    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_clock = pygame.time.Clock()
     dt = 0
+
+    player_1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
         
@@ -18,6 +19,7 @@ def main():
                 return
         
         screen.fill("black")
+        player_1.draw(screen)
         pygame.display.flip()
         
         dt = game_clock.tick(60) / 1000
